df2bf50dba09b5585e9bc0f8e72c921f184c0a8e
yuuidicn <154424441+yuuidicn@users.noreply.github.com>
2024-05-23 01:25:14 +0800

    Update baseIndex.js

--- web-user/src/views/Home/baseIndex.js
+++ web-user/src/views/Home/baseIndex.js
@@ -15,10 +15,10 @@ const BaseIndex = () => (
       <Grid container columns={12} wrap="nowrap" alignItems="center" sx={{ minHeight: 'calc(100vh - 230px)' }}>
         <Grid md={7} lg={6}>
           <Stack spacing={3}>
-            <Typography variant="h1" sx={{ fontSize: '4rem', color: '#fff', lineHeight: 1.5 }}>
+            <Typography variant="h1" sx={{ fontSize: '4rem', color: '#050828', lineHeight: 1.5 }}>
               Chat API
             </Typography>
-            <Typography variant="h4" sx={{ fontSize: '1.5rem', color: '#fff', lineHeight: 1.5 }}>
+            <Typography variant="h4" sx={{ fontSize: '1.5rem', color: '#050828', lineHeight: 1.5 }}>
               All in one 的 OpenAI 接口 <br />
               整合各种 API 访问方式 <br />
               一键部署，开箱即用
@@ -28,7 +28,7 @@ const BaseIndex = () => (
               startIcon={<GitHub />}
               href="https://github.com/ai365vip/chat-api"
               target="_blank"
-              sx={{ backgroundColor: '#24292e', color: '#fff', width: 'fit-content', boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)' }}
+              sx={{ backgroundColor: '#24292e', color: '#050828', width: 'fit-content', boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)' }}
             >
               GitHub
             </Button>
